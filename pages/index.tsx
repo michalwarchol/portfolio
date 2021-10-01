da4da809b44ffdf29be532b25ca9e4e08b6d8353
Michał Warchoł <imsimilar@gmail.com>
2021-10-01 23:30:31 +0200

    :a: imported Monsterrat font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
-import type { NextPage } from 'next'
-import Header from '../src/Header/Header'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Content from "../src/components/Content/Content";
+import Header from "../src/components/Header/Header";
 
 const Home: NextPage = () => {
   return (
     <div>
-        <Header />
+      <Head>
+        <title>Michał Warchoł</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Header />
+      <Content />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
