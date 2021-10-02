6c43c0d743ba1ef15593b21226e066e959dfd6f3
Michał Warchoł <imsimilar@gmail.com>
2021-10-02 21:02:32 +0200

    :zap: Removed unnecessary buttons and added blue cricle

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import GlassPanel from '../GlassPanel/GlassPanel';
 import styles from "./Header.module.scss";
-import { BsFillGearFill, BsWrench } from "react-icons/bs";
-import {GoPencil} from "react-icons/go"
 
 const Header:React.FC = () => {
     return(
@@ -15,23 +12,7 @@ const Header:React.FC = () => {
                     Michał Warchoł - fullstack developer
                 </h2>
             </div>
-            <div className={styles.panels}>
-            <GlassPanel animationDelay={"0.5s"} MyIcon={BsFillGearFill}>
-                <h1>
-                    Projects
-                </h1>
-            </GlassPanel>
-            <GlassPanel animationDelay={"0.7s"} MyIcon={BsWrench}>
-                <h1>
-                    TechStack
-                </h1>
-            </GlassPanel>
-            <GlassPanel animationDelay={"0.9s"} MyIcon={GoPencil}>
-                <h1>
-                    About Me
-                </h1>
-            </GlassPanel>
-            </div>
+            <div className={styles.blueCircle}></div>
         </header>
     )
 }
