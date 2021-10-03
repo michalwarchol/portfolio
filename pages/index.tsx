356a82ecb748fac16faa7848b6de202d3d8581a0
Michał Warchoł <imsimilar@gmail.com>
2021-10-03 21:43:36 +0200

    :art: Changed background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,21 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Content from "../src/components/Content/Content";
+import styles from "./index.module.scss";
 import Header from "../src/components/Header/Header";
+import Projects from "../src/components/Projects/Projects";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className={styles.index}>
       <Head>
         <title>Michał Warchoł</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;900&display=swap"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500&display=swap" rel="stylesheet" />
       </Head>
       <Header />
-      <Content />
+      <Projects />
     </div>
   );
 };
