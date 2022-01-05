c1bc84f9bf4d5807b16455cb101fb516a5c7bda3
Michał Warchoł <imsimilar@gmail.com>
2022-01-05 06:25:09 +0100

    :broom: Refactored

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,22 +1,20 @@
-import React from 'react'
+import React from "react";
 import styles from "./List.module.scss";
 
 interface Props {
-    title: string;
+  title: string;
 }
 
-const List:React.FC<Props> = ({title, children}) => {
-    return(
-        <div className={styles.article}>
-            <div className={styles.innerArticle}>
-                <div className={styles.title}>
-                    <span>{title}</span>
-                </div>
-                <div>
-                {children}
-                </div>
-            </div>
+const List: React.FC<Props> = ({ title, children }) => {
+  return (
+    <div className={styles.article}>
+      <div className={styles.innerArticle}>
+        <div className={styles.title}>
+          <span>{title}</span>
         </div>
-    )
-}
+        <div>{children}</div>
+      </div>
+    </div>
+  );
+};
 export default List;
